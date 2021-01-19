d2bca95230d7b29544b3f08e66712c650c04324e
ryan <ryan@pop-os.localdomain>
2021-01-19 17:08:25 +0700

    [IMP] Rename package to match egg

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
-setup(name='odoo-import-export-client',
+setup(name='odoo-csv-tools',
       version='3.0.0',
       install_requires=['odoo-client-lib', 'future', 'unicodecsv', 'requests'],
       description='Library and script that allow to export and import data to Odoo using rpc api.',
